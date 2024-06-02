6d835d512b93da9a38ff804fa1f57146ae32d67e
Vigneshwar Boggula <vboggula@uci.edu>
2024-06-02 00:28:37 -0700

    song component

---
+++ src/app/song/song.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-song',
+  templateUrl: './song.component.html',
+  styleUrls: ['./song.component.css']
+})
+export class SongComponent {
+
+}
