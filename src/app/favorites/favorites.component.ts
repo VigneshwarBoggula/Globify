62abbe456c1162c77c2490b847044052f81acc27
Vigneshwar Boggula <vboggula@uci.edu>
2024-06-02 14:07:54 -0700

    favorites list tile

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./favorites.component.css']
 })
 export class FavoritesComponent {
+  favoriteSongs: { title: string, artist: string, selected: boolean }[] = [
+    { title: 'Not Like Us', artist: 'Kendrick Lamar', selected: false },
+    { title: 'Lunch', artist: 'Billie Eilish', selected: false },
+  ];
 
+  removeSong(index: number) {
+    this.favoriteSongs.splice(index, 1);
+  }
+
+  toggleSelection(index: number) {
+    this.favoriteSongs.forEach((song, i) => {
+      if (i !== index) {
+        song.selected = false;
+      }
+    });
+
+    this.favoriteSongs[index].selected = !this.favoriteSongs[index].selected;
+  }
 }
